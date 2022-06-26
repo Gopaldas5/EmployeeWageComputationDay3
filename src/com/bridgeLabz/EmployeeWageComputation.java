0f7f75bbe544f5970d72ea637835b7746928b01b
GopalDas <gopalkrdas1997@gmail.com>
2022-06-26 15:37:06 +0530

    ADDED: Calculating the part time wage of employee

--- src/com/bridgeLabz/EmployeeWageComputation.java
+++ src/com/bridgeLabz/EmployeeWageComputation.java
@@ -6,29 +6,32 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program ");
         Random random = new Random();
-        int attendance = random.nextInt(2);
-        int IS_PRESENT_TODAY = 1;
+        int attendance = random.nextInt(3);
+        int IS_PRESENT_DAY = 1;
+        int IS_ABSENT_DAY = 0;
+        int IS_HALF_DAY = 2;
         int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
-        int ABSENT_DAY_HOUR = 0;
+        int FULL_DAY_TIME = 8;
+        int HALF_DAY_TIME = 4;
         int TOTAL_EMP_WAGE = 0;
 
-        if (attendance == IS_PRESENT_TODAY){
-            TOTAL_EMP_WAGE = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println(" Employee is Present");
+        if (attendance == IS_PRESENT_DAY){
+            TOTAL_EMP_WAGE = WAGE_PER_HOUR * FULL_DAY_TIME;
+            System.out.println(" Employee is Present ");
+            System.out.println(" Employee is working full day");
+            System.out.println(" Total employee wage:- "+TOTAL_EMP_WAGE);
+        }
+        else if(attendance ==IS_HALF_DAY){
+            TOTAL_EMP_WAGE = WAGE_PER_HOUR * HALF_DAY_TIME;
+            System.out.println("Employee is present ");
+            System.out.println("Employee is working half day");
             System.out.println("Total employee wage:- "+TOTAL_EMP_WAGE);
-
         }
         else{
-            TOTAL_EMP_WAGE = WAGE_PER_HOUR * ABSENT_DAY_HOUR;
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is absent");
             System.out.println("Total employee wage:- "+TOTAL_EMP_WAGE);
 
         }
-
-
-
     }
-
 
 }
