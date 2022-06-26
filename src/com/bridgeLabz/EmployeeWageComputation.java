66e77d47d06a3dc33c4d0b06639f5b0f45fac9cd
GopalDas <gopalkrdas1997@gmail.com>
2022-06-26 15:08:48 +0530

    ADDED: Calculating the total wages of employee

--- src/com/bridgeLabz/EmployeeWageComputation.java
+++ src/com/bridgeLabz/EmployeeWageComputation.java
@@ -8,14 +8,26 @@ public class EmployeeWageComputation {
         Random random = new Random();
         int attendance = random.nextInt(2);
         int IS_PRESENT_TODAY = 1;
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
+        int ABSENT_DAY_HOUR = 0;
+        int TOTAL_EMP_WAGE = 0;
 
         if (attendance == IS_PRESENT_TODAY){
+            TOTAL_EMP_WAGE = WAGE_PER_HOUR * FULL_DAY_HOUR;
             System.out.println(" Employee is Present");
-        }
-        else{
-            System.out.println("Employee is Absent");
+            System.out.println("Total employee wage:- "+TOTAL_EMP_WAGE);
 
         }
+        else{
+            TOTAL_EMP_WAGE = WAGE_PER_HOUR * ABSENT_DAY_HOUR;
+            System.out.println("Employee is Absent");
+            System.out.println("Total employee wage:- "+TOTAL_EMP_WAGE);
+
+        }
+
+
+
     }
 
 
